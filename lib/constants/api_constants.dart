92742062a37d5366eff16ab43b515d2a27f26e3e
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-03-24 21:48:24 -0400

    location bg

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,4 +1,4 @@
-const BASE = 'https://safertynet-server.herokuapp.com/';
+const BASE = 'https://safetynet-server.herokuapp.com/';
 const UPDATE_LOCATION = BASE + 'updatelocation';
 const ALERT = BASE + 'alert';
 const NEW_DEVICE = BASE + 'new';
